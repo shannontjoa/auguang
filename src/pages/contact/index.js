9636877257e268f05bb654982f7674d4312dbe11
Shannon Tjoa <emkachaka@gmail.com>
2019-03-17 22:24:41 +1100

    Change nav-bar to fixed position (#7)

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,40 +1,14 @@
 import React from 'react';
-import { navigate } from 'gatsby-link';
 import Layout from '../../components/Layout';
 import MdHome from 'react-icons/lib/md/home';
 import FaPhone from 'react-icons/lib/fa/phone';
 import MdEmail from 'react-icons/lib/md/email';
-
-function encode(data) {
-  return Object.keys(data)
-    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&');
-}
 
 export default class Index extends React.Component {
   constructor(props) {
     super(props);
     this.state = { isValidated: false };
   }
-
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    const form = e.target;
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({
-        'form-name': form.getAttribute('name'),
-        ...this.state,
-      }),
-    })
-      .then(() => navigate(form.getAttribute('action')))
-      .catch(error => alert(error));
-  };
 
   render() {
     return (
@@ -43,7 +17,7 @@ export default class Index extends React.Component {
           <div className="container">
             <div className="content">
               <h1>Contact Us</h1>
-              <div className="card">
+              <div className="card" style={{ 'max-width': '400px' }}>
                 <div className="card-image" />
                 <div className="card-content">
                   <MdHome />
